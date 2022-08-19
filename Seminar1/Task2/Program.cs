73fae6ffee5cdf7beb1c6776010f00cfc3430333
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-19 18:24:03 +0300

    Task2

--- Seminar1/Task2/Program.cs
+++ Seminar1/Task2/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
