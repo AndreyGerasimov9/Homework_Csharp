c88528f8b661f0371ddc441353a20c94bf127d1e
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-24 21:23:14 +0300

    Task2

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int start = 1;
+int stop = 65537;
+int value = new Random().Next(start, stop);
+Console.WriteLine(value);
+int digit_third = 0;
+if (value < 100000)
+{
+    digit_third = value/100%10;    
+} 
+if (value < 10000)
+{
+    digit_third = value/10%10;
+}
+if (value < 1000)
+{
+    digit_third = value%10;
+}
+if (value < 100)
+{
+    digit_third = 0;
+    Console.WriteLine("третьей цифры нет");
+}
+Console.WriteLine(digit_third);
