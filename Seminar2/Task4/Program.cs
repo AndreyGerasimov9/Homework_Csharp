a7a9bdc95dd86bed9d7448d5ba78d467fe45cbec
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-25 00:09:25 +0300

    Task4

---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int start = 1;
+int stop = 1000000;
+int value = new Random().Next(start, stop);
+Console.WriteLine(value);
+if (value%7 == 0)
+{
+    if (value%23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+}
+else
+{
+    Console.WriteLine("нет");
+}
