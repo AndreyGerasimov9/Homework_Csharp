092eeff9bb84c4230da2319b0b8d63d1d956688c
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-24 23:30:40 +0300

    Task3

---
+++ Seminar2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели от 1 до 7, чтобы проверить, является ли этот день выходным: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+switch (x)
+{
+    case 6: Console.WriteLine("да");
+    break;
+    case 7: Console.WriteLine("да");
+    break;
+    default: Console.WriteLine("нет");
+    break;
+}
