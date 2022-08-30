03b782d3c4134cae39f9862993854ab58c6e549f
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-31 01:54:18 +0300

    Task2

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе, которые являются делителями этого числа.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int temporary = number;
+
+int sum = 0;
+while (temporary > 0)
+{
+    int digit = temporary%10;
+    if (digit == 0)
+        break;
+        if (number%digit == 0)
+        {
+            sum = sum + digit;
+        }
+    temporary = temporary/10;
+}
+Console.WriteLine(sum);
