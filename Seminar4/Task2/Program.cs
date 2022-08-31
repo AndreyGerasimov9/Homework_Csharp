4fdc8df2fc3b548bc8c32c4847cff3b8346ff884
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-08-31 10:58:25 +0300

    Final

--- Seminar4/Task2/Program.cs
+++ Seminar4/Task2/Program.cs
@@ -3,19 +3,19 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 int temporary = number;
-
 int sum = 0;
 while (temporary > 0)
 {
     int digit = temporary%10;
-    if (digit == 0)
-        break;
+    if (digit != 0)
+    {
         if (number%digit == 0)
         {
             sum = sum + digit;
         }
+    }
     temporary = temporary/10;
+    
 }
 Console.WriteLine(sum);
