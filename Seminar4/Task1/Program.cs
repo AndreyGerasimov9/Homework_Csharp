b37dda86e362bd3f5203b530a07384d5524766b2
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-09-01 23:16:20 +0300

    Final

--- Seminar4/Task1/Program.cs
+++ Seminar4/Task1/Program.cs
@@ -4,8 +4,8 @@ Console.Write("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
-int result = A;
-for (int i = 1; i < B; i++)
+int result = 1;
+for (int i = 0; i < B; i++)
 {
     result = result * A;
 }
