4f831c1a62fe20584438f479789077666221c1bc
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-09-10 23:06:19 +0300

    Task1

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными (тип double) числами.
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        twoDimArray[i, j] = rnd.Next(-10, 10) + rnd.NextDouble();
+        twoDimArray[i, j] = Math.Round(twoDimArray[i, j], 1);
+        Console.Write(twoDimArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
