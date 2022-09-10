65bf330942454aa2f02a37302866e8f40eb858c5
AndreyGerasimov9 <angerasimov9@gmail.com>
2022-09-11 01:26:49 +0300

    Task3

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] twoDimArray = new int[m, n];
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        twoDimArray[i, j] = rnd.Next(1, 10);
+        Console.Write(twoDimArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+double average = 0;
+for (int j = 0; j < n; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + twoDimArray[i, j];
+        average = sum/m;
+        average = Math.Round(average, 1);
+    }
+    Console.Write(average + "; ");
+}
+Console.WriteLine();
